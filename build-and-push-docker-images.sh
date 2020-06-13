4b2bd8ff21938651408c9fd78f2435ebdfecd7b5
Joran Bergfeld <joran.bergfeld@devoteam.com>
2020-06-13 09:51:53 +0200

    Add experimental feature to speed build. Update to push with version.

--- build-and-push-docker-images.sh
+++ build-and-push-docker-images.sh
@@ -3,20 +3,22 @@ set -e
 function build-and-push-docker-for-service {
   MODULE_DIRECTORY=$1
   MODULE_NAME=$2
-  cd $1
-  docker build . -t $2
-  docker tag $2 "$DOCKER_HUB_USERNAME"/$2:v1
-  docker push "$DOCKER_HUB_USERNAME"/$2:v1
+  VERSION=$3
+  cd $MODULE_DIRECTORY
+  docker build . -t $MODULE_NAME
+  docker tag $MODULE_NAME "$DOCKER_HUB_USERNAME"/$MODULE_NAME:v$VERSION
+  docker push "$DOCKER_HUB_USERNAME"/$MODULE_NAME:v$VERSION
   cd ..
 }
 
 DOCKER_HUB_USERNAME=$1
 DOCKER_HUB_PASSWORD=$2
+VERSION=$3
 
 docker login -u "$DOCKER_HUB_USERNAME" -p "$DOCKER_HUB_PASSWORD"
 
-build-and-push-docker-for-service order-service order-app
-build-and-push-docker-for-service payment-service payment-app
-build-and-push-docker-for-service product-service product-app
-build-and-push-docker-for-service stock-service stock-app
-build-and-push-docker-for-service user-service user-app
+build-and-push-docker-for-service order-service order-app $VERSION
+build-and-push-docker-for-service payment-service payment-app $VERSION
+build-and-push-docker-for-service product-service product-app $VERSION
+build-and-push-docker-for-service stock-service stock-app $VERSION
+build-and-push-docker-for-service user-service user-app $VERSION
